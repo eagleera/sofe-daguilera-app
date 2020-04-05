c85522fa1d9857fc9bd96e1862cffbdb7e394c56
Daniel Aguilera <daguilera3220@gmail.com>
2020-04-05 12:25:33 -0600

    se agregan request https

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,14 +3,14 @@ import fetch from "fetch";
 
 export default class IndexRoute extends Route {
   async model() {
-    const responsestats = await fetch("http://api.coronastatistics.live/all");
+    const responsestats = await fetch("https://api.coronastatistics.live/all");
     const stats = await responsestats.json();
     const responsecountries = await fetch(
-      "http://api.coronastatistics.live/countries"
+      "https://api.coronastatistics.live/countries"
     );
     const countries = await responsecountries.json();
     const responsetimeline = await fetch(
-      "http://api.coronastatistics.live/timeline/global"
+      "https://api.coronastatistics.live/timeline/global"
     );
     const timeline = await responsetimeline.json();
     return { stats, countries, timeline };
